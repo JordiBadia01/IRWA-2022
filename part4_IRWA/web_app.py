66ae265c9d8a3bdcbc096946e008e6aecbd98038
Cristian Bassotto <cristianbassotto21@gmail.com>
2022-12-11 17:54:29 +0100

    update

--- part4_IRWA/web_app.py
+++ part4_IRWA/web_app.py
@@ -1,13 +1,15 @@
+import datetime
 import os
 from json import JSONEncoder
+import time
 
-#pip install httpagentparser
+# pip install httpagentparser
 import httpagentparser  # for getting the user agent as json
 import nltk
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, jsonify
 from flask import request
 
-from myapp.analytics.analytics_data import AnalyticsData, ClickedDoc
+from myapp.analytics.analytics_data import AnalyticsData, ClickedDoc, user_context, results_data
 from myapp.search.load_corpus import load_corpus
 from myapp.search.objects import Document, StatsDocument
 from myapp.search.search_engine import SearchEngine
@@ -31,11 +33,6 @@ app.secret_key = 'afgsreg86sr897b6st8b76va8er76fcs6g8d7'
 # open browser dev tool to see the cookies
 app.session_cookie_name = 'IRWA_SEARCH_ENGINE'
 
-
-
-# instantiate our in memory persistence
-analytics_data = AnalyticsData()
-
 # print("current dir", os.getcwd() + "\n")
 # print("__file__", __file__ + "\n")
 full_path = os.path.realpath(__file__)
@@ -51,30 +48,40 @@ print("loaded corpus. first elem:", list(corpus.values())[0])
 # instantiate our search engine
 search_engine = SearchEngine(corpus, dump_path)
 
+# instantiate our in memory persistence
+analytics_data = AnalyticsData(dump_path)
+
+
 # Home URL "/"
 @app.route('/')
 def index():
     print("starting home url /...")
 
+    if 'details' in session and 'doc_details_time' in session and session['details']:
+        update_dwell()
+
     # flask server creates a session by persisting a cookie in the user's browser.
     # the 'session' object keeps data between multiple requests
-    session['some_var'] = "IRWA 2022 home"
+    user_cont = user_context(request)
+    user = user_cont.user_ip
+    analytics_data.ip2user[user] = user_cont
 
-    user_agent = request.headers.get('User-Agent')
-    print("Raw user browser:", user_agent)
+    print("Remote IP: {} - JSON user browser {}".format(user, user_cont.agent))
 
-    user_ip = request.remote_addr
-    agent = httpagentparser.detect(user_agent)
-
-    print("Remote IP: {} - JSON user browser {}".format(user_ip, agent))
-
-    print(session)
+    if user in analytics_data.fact_users.keys():
+        analytics_data.fact_users[user].append([datetime.datetime.now(), []])
+    else:
+        analytics_data.fact_users[user] = [[datetime.datetime.now(), []]]
 
     return render_template('index.html', page_title="Welcome")
 
 
 @app.route('/search', methods=['POST'])
 def search_form_post():
+    analytics_data.save()
+    if 'details' in session and 'doc_details_time' in session and session['details']:
+        update_dwell()
+
     search_query = request.form['search-query']
 
     session['last_search_query'] = search_query
@@ -84,43 +91,79 @@ def search_form_post():
     results = search_engine.search(search_query, search_id, corpus)
 
     found_count = len(results)
-    session['last_found_count'] = found_count
 
-    print(session)
+    session['last_found_count'] = found_count
+    session['last_search_query_id'] = search_id
+
+    # time based sessions: queries of a user in the same sit down (physical
+    # session).
+    user_cont = user_context(request)
+    user = user_cont.user_ip
+    analytics_data.ip2user[user] = user_cont
+
+    if user in analytics_data.fact_users.keys():
+        analytics_data.fact_users[user][len(analytics_data.fact_users[user])-1][1].append(search_id)
+    else:
+        analytics_data.fact_users[user] = [[datetime.datetime.now(), [search_id]]]
+
+    # clicks on documents.
+    # to what query where related.
+    # ranking of clicked documents.
+    for results_doc in results:
+        doc_id = results_doc.id
+        ranking = results_doc.ranking
+        if doc_id not in analytics_data.fact_results.keys():
+            analytics_data.fact_results[doc_id] = results_data()
+        analytics_data.fact_results[doc_id].query.append((search_id, ranking))
 
     return render_template('results.html', results_list=results, page_title="Results", found_counter=found_count)
 
 
 @app.route('/doc_details', methods=['GET'])
 def doc_details():
+    analytics_data.save()
     # getting request parameters:
     # user = request.args.get('user')
+    if 'details' in session and 'doc_details_time' in session and session['details']:
+        update_dwell()
 
-    print("doc details session: ")
-    print(session)
-
-    res = session["some_var"]
-
-    print("recovered var from session:", res)
+    session['details'] = True
 
     # get the query string parameters from request
     clicked_doc_id = int(request.args["id"])
-    p1 = int(request.args["search_id"])  # transform to Integer
+    search_id = int(request.args["search_id"])  # transform to Integer
     p2 = int(request.args["param2"])  # transform to Integer
     print("click in id={}".format(clicked_doc_id))
 
+    session['doc_details'] = clicked_doc_id
+    session['doc_details_time'] = time.time()
     # object of clicked doc (to sent to doc_details page)
     doc = corpus[clicked_doc_id]
 
     # store data in statistics table 1
-    if clicked_doc_id in analytics_data.fact_clicks.keys():
-        analytics_data.fact_clicks[clicked_doc_id] += 1
+    if clicked_doc_id in analytics_data.fact_results.keys():
+        if search_id in analytics_data.fact_results[clicked_doc_id].clicks.keys():
+            analytics_data.fact_results[clicked_doc_id].clicks[search_id] += 0.5
+        else:
+            analytics_data.fact_results[clicked_doc_id].clicks[search_id] = 0.5
     else:
-        analytics_data.fact_clicks[clicked_doc_id] = 1
+        analytics_data.fact_results[clicked_doc_id] = results_data()
+        analytics_data.fact_results[clicked_doc_id].clicks[search_id] = 0.5
 
-    print("fact_clicks count for id={} is {}".format(clicked_doc_id, analytics_data.fact_clicks[clicked_doc_id]))
+    print("fact_results clicks count for id={} is {}".format(clicked_doc_id,
+                                                             analytics_data.fact_results[clicked_doc_id].clicks))
 
-    return render_template('doc_details.html', doc = doc)
+    return render_template('doc_details.html', doc=doc, page_title="Details")
+
+
+def update_dwell():
+    doc = session['doc_details']
+    last_time = session['doc_details_time']
+    current_time = time.time()
+    if doc not in analytics_data.fact_results.keys():
+        analytics_data.fact_results[doc] = results_data()
+    analytics_data.fact_results[doc].dwell_time.append(current_time - last_time)
+    session['details'] = False
 
 
 @app.route('/stats', methods=['GET'])
@@ -129,51 +172,60 @@ def stats():
     Show simple statistics example. ### Replace with dashboard ###
     :return:
     """
+    analytics_data.save()
+    if 'details' in session and 'doc_details_time' in session and session['details']:
+        update_dwell()
 
     docs = []
     # ### Start replace with your code ###
 
-    for doc_id in analytics_data.fact_clicks:
+    for doc_id in analytics_data.fact_results.keys():
         row: Document = corpus[int(doc_id)]
-        count = analytics_data.fact_clicks[doc_id]
-        doc = StatsDocument(row.id, row.title, row.description, row.doc_date, row.url, count)
-        docs.append(doc)
-
-    # simulate sort by ranking
+        count = sum(list(analytics_data.fact_results[doc_id].clicks.values()))
+        if count >0:
+            doc = StatsDocument(row.id, row.title, row.description, row.doc_date, row.url, count)
+            docs.append(doc)
+        # simulate sort by ranking
     docs.sort(key=lambda doc: doc.count, reverse=True)
-    return render_template('stats.html', clicks_data=docs)
+
+    users = []
+    for user in analytics_data.fact_users:
+        data = analytics_data.fact_users[user][len(analytics_data.fact_users[user])-1][0]
+        queries = [analytics_data.fact_queries[query] for query in analytics_data.fact_users[user][len(analytics_data.fact_users[user])-1][1]]
+        users.append((user, data, queries))
+
+    terms = sorted(analytics_data.fact_terms.items(), key=lambda x: x[1], reverse=True)
+
+    return render_template('stats.html', clicks_data=docs[:10], users=users[:10], terms= terms[:10], page_title='Statistics')
     # ### End replace with your code ###
 
 
 @app.route('/dashboard', methods=['GET'])
 def dashboard():
+    analytics_data.save()
+    if 'details' in session and 'doc_details_time' in session and session['details']:
+        update_dwell()
+
     visited_docs = []
-    print(analytics_data.fact_clicks.keys())
-    for doc_id in analytics_data.fact_clicks.keys():
+    print(analytics_data.fact_results.keys())
+    for doc_id in analytics_data.fact_results.keys():
         d: Document = corpus[int(doc_id)]
-        doc = ClickedDoc(doc_id, d.description, analytics_data.fact_clicks[doc_id])
-        visited_docs.append(doc)
+        doc = ClickedDoc(doc_id, d.description, sum(list(analytics_data.fact_results[doc_id].clicks.values())))
+        if doc.counter > 0:
+            visited_docs.append(doc)
 
     # simulate sort by ranking
     visited_docs.sort(key=lambda doc: doc.counter, reverse=True)
-    visited_ser=[]
+    visited_ser = []
     for doc in visited_docs:
         visited_ser.append(doc.to_json())
-    return render_template('dashboard.html', visited_docs=visited_ser)
 
-@app.route('/sentiment')
-def sentiment_form():
-    return render_template('sentiment.html')
+    terms = sorted(analytics_data.fact_terms.items(),  key=lambda x: x[1], reverse=True)
+    terms_ser = []
+    for term in terms:
+        terms_ser.append((term[0], term[1]))
 
-
-@app.route('/sentiment', methods=['POST'])
-def sentiment_form_post():
-    text = request.form['text']
-    nltk.download('vader_lexicon')
-    from nltk.sentiment.vader import SentimentIntensityAnalyzer
-    sid = SentimentIntensityAnalyzer()
-    score = ((sid.polarity_scores(str(text)))['compound'])
-    return render_template('sentiment.html', score=score)
+    return render_template('dashboard.html', visited_docs=visited_ser[:10], visited_terms= terms_ser, page_title='Dashboard')
 
 
 if __name__ == "__main__":
